32bd698cf46696784e4922db57f8f7b2e520ec68
Masuda-Jion <dgg2>
2023-02-18 20:22:50 +0000

    [add] プロフィールのバリデーション

--- Bookers2/app/controllers/users_controller.rb
+++ Bookers2/app/controllers/users_controller.rb
@@ -5,21 +5,25 @@ class UsersController < ApplicationController
   end
 
   def index
-   
+    @user = current_user
+    @users = User.all
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
